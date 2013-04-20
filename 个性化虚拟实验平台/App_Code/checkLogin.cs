5b5320eedd46842e93e10b3aaf5d8942198d3ffe
Watteau <liangtuohua@163.com>
2013-04-20 17:00:44 +0800

    Begin at 2013.4.20
    Begin at 2013.4.20

---
+++ 个性化虚拟实验平台/App_Code/checkLogin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+///checkLogin 的摘要说明
+/// </summary>
+public class checkLogin
+{
+	public checkLogin()
+	{
+		//
+		//TODO: 在此处添加构造函数逻辑
+		//
+	}
+}
